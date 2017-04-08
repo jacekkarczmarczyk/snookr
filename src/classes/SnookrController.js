04295a59c2ca73fe1d12f8bd71b4140b7057f353
jk <jacek@karczmarczyk.pl>
2017-04-08 21:42:33 +0700

    ...

--- src/classes/SnookrController.js
+++ src/classes/SnookrController.js
@@ -5,7 +5,7 @@ class SnookrController {
      */
     constructor(playerNames) {
         this.gameId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            const r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         });
         this.gameState = {
