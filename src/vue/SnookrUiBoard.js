1db2444a9f3cf85c04e82d6f0af0e8f48f0d15a2
jk <jacek@karczmarczyk.pl>
2017-04-12 17:24:05 +0700

    score board layout

--- src/vue/SnookrUiBoard.js
+++ src/vue/SnookrUiBoard.js
@@ -4,8 +4,9 @@ Vue.component('snookr-ui-board', {
     template: `
 <div class="snookr-ui-board">
     <snookr-ui-player-panel v-bind:player="gameState.players[0]" v-bind:current="gameState.currentPlayer === 0" v-bind:rule="gameState.currentRule" v-bind:is-snooker="gameState.isSnooker"></snookr-ui-player-panel>
-    <snookr-ui-spinner-ball v-bind:spin-power="gameState.spinPower"></snookr-ui-spinner-ball>
+    <div class="snookr-ui-board__frame-count-panel">(7)</div>
     <snookr-ui-player-panel v-bind:player="gameState.players[1]" v-bind:current="gameState.currentPlayer === 1" v-bind:rule="gameState.currentRule" v-bind:is-snooker="gameState.isSnooker"></snookr-ui-player-panel>
+    <snookr-ui-spinner-ball v-bind:spin-power="gameState.spinPower"></snookr-ui-spinner-ball>
 </div>`,
     props: ['gameState'],
     data: () => ({})
