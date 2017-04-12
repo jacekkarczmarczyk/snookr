73ea39422de6448af69111a1772a7a6eff3f6bd7
jk <jacek@karczmarczyk.pl>
2017-04-12 23:35:35 +0700

    sounds

--- src/classes/AudioClip.js
+++ src/classes/AudioClip.js
@@ -1,22 +1,28 @@
 class AudioClip {
-    constructor(domElement, start = 0, length = 0) {
-        this.domElement = domElement;
+    constructor(src, start = 0, length = 0) {
+        this.src = src;
         this.start = start / 1000;
         this.length = length;
     }
 
     play(volume = 1) {
-        let audio = this.domElement.ownerDocument.createElement('audio');
+        let audio = document.createElement('audio');
 
-        audio.src = this.domElement.getAttribute('src');
+        audio.src = this.src;
         audio.volume = volume;
         audio.currentTime = this.start;
-        audio.play();
 
-        this.length && window.setTimeout(function () {
-            audio.pause();
-            audio.currentTime = 0;
-            audio = null;
-        }, this.length);
+        const self = this;
+        return new Promise(function (resolve) {
+            audio.play();
+            audio.addEventListener('ended', () => resolve());
+            self.length && window.setTimeout(function () {
+                audio.pause();
+                resolve();
+                audio.currentTime = 0;
+                audio = null;
+            }, self.length);
+        });
+
     }
 }
--- src/classes/SnookrAudioPlayer.js
+++ src/classes/SnookrAudioPlayer.js
@@ -1,25 +1,31 @@
 class SnookrAudioPlayer {
     constructor() {
-        const applause1Element = document.querySelector('audio[data-type="applause1"]');
-        const applause2Element = document.querySelector('audio[data-type="applause2"]');
-        const disappointmentElement = document.querySelector('audio[data-type="disappointment"]');
-        const ballElement = document.querySelector('audio[data-type="ball"]');
-
         this.clips = {
             applause: [
-                new AudioClip(applause1Element, 1900),
-                new AudioClip(applause2Element, 400),
+                new AudioClip('audio/new/a1.mp3'),
+                new AudioClip('audio/new/a2.mp3'),
+                new AudioClip('audio/new/a3.mp3'),
+                new AudioClip('audio/new/a4.mp3'),
+                new AudioClip('audio/new/a5.mp3'),
+                new AudioClip('audio/new/a6.mp3'),
+                new AudioClip('audio/new/a7.mp3'),
+                new AudioClip('audio/new/a8.mp3'),
             ],
-            disappointment: new AudioClip(disappointmentElement, 600),
-            cueHitsBall: new AudioClip(ballElement, 2400, 300),
-            ballHitsBall: new AudioClip(ballElement, 2950, 300),
-            ballHitsPocket: new AudioClip(ballElement, 3300, 300),
+            background: new AudioClip('audio/new/background.mp3'),
+            disappointment: new AudioClip('audio/disappointment.mp3', 600),
+            cueHitsBall: new AudioClip('audio/ball.mp3', 2400, 300),
+            ballHitsBall: new AudioClip('audio/ball.mp3', 2950, 300),
+            ballHitsPocket: new AudioClip('audio/ball.mp3', 3300, 300),
         };
+    }
+
+    playBackgroundSilence() {
+        this.clips.background.play(0.4).then(() => this.playBackgroundSilence());
     }
 
     playApplause() {
         const applauseAudioClips = this.clips.applause;
-        applauseAudioClips[Math.floor(Math.random() * applauseAudioClips.length)].play(0.1);
+        applauseAudioClips[Math.floor(Math.random() * applauseAudioClips.length)].play(0.15);
     }
 
     playDisappointment() {
